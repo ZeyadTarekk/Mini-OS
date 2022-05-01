5e2938417482b0e7903fc7873b21dd281364dbb2
BeshoyMorad <besh0morta@gmail.com>
2022-05-01 22:19:16 +0200

    Added the process struct

--- headers.h
+++ headers.h
@@ -65,3 +65,34 @@ void destroyClk(bool terminateAll)
         killpg(getpgrp(), SIGINT);
     }
 }
+
+
+//the struct of process
+struct ProcessStruct
+{
+    //info from the file
+    int id;
+    int arrivalTime;
+    int priority;
+    int runTime;
+
+    bool running;
+
+    // if equals false then create new process
+    // else wake the process up
+    bool startedBefore;
+
+    // if (enterQueue == 0) enterQueue = arrivalTime
+    // else enterQueue = clk;
+    int enterQueue;
+
+    // quitQueue = clk;
+    int quitQueue;
+
+    // executionTime = executionTime + (clk - quitQueue)
+    int executionTime;
+
+    // waitingTime = clk - enterQueue;
+    int waitingTime;
+
+};
