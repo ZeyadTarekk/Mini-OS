8ae69a27d1f1bbf2545196b62788f6ec8842b237
abdelrahman <a.m.hamza156@gmail.com>
2022-05-06 01:56:49 +0200

    modify scheduler handler and main function

--- scheduler.c
+++ scheduler.c
@@ -1,8 +1,12 @@
-#include "headers.h"
+//#include "headers.h"
+#include "SRTN.h"
 
 // global variables
 int msgq_id;
+int algorithm;
 struct msgbuff message;
+struct PQueue *priority_queue;
+struct Queue *queue;
 
 void getProcess(int);
 
@@ -10,8 +14,7 @@ void intializeMessageQueue() {
     key_t key_id = ftok("keyfile", PROSCH);
     msgq_id = msgget(key_id, 0666 | IPC_CREAT);
 
-    if (msgq_id == -1)
-    {
+    if (msgq_id == -1) {
         perror("Error in create message queue");
         exit(-1);
     }
@@ -21,11 +24,9 @@ void intializeMessageQueue() {
 bool flag = true;
 
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char *argv[]) {
     //TODO implement the scheduler :)
     initClk();
-
     //add signal handler to get the processes from process_generator
     signal(SIGUSR1, getProcess);
 
@@ -34,17 +35,42 @@ int main(int argc, char * argv[])
 
 
     //get the algorithm number
-    int algorithm = atoi(argv[1]);
+    algorithm = atoi(argv[1]);
 
-    while (flag)
-    {
-        
+    switch (algorithm) {
+        case 1:
+            // Allocate the priority queue
+            priority_queue = createPriorityQueue();
+
+            // TODO: Add your algorithm call here (HPF)
+
+            break;
+        case 2:
+            // Allocate the priority queue
+            priority_queue = createPriorityQueue();
+
+            // Call the algorithm function
+            SRTN(priority_queue);
+
+            break;
+        case 3:
+            // Allocate the queue
+            queue = createQueue();
+
+            // TODO: Add your algorithm call here (RR)
+
+            break;
     }
-    
+
     //upon termination release the clock resources.
     destroyClk(false);
 
     return 0;
+}
+
+void add_to_SRTN_queue(struct ProcessStruct process) {
+    // Push to the queue and the priority is the runTime (The remaining time at the beginning)
+    push(priority_queue, &process, process.runTime);
 }
 
 void getProcess(int signum) {
@@ -54,10 +80,19 @@ void getProcess(int signum) {
     if (rec_val == -1)
         perror("Error in receive");
 
-    printf("Schedular received process with id = %d\n", message.process.id);
+    // For testing only
+    __SRTN_print_process_info(&message.process);
+    switch (algorithm) {
+        case 1:
+            // TODO: Add to [PRIORITY QUEUE] as HPF
 
-    if (message.process.id == 3)
-    {
-        flag = false;
+            break;
+        case 2:
+            // DONE: Add to priority queue as SRTN
+            add_to_SRTN_queue(message.process);
+        case 3:
+            // TODO: Add to [QUEUE] as RR
+
+            break;
     }
 }
