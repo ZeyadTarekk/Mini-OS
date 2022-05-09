d1a06b8a6ce2d5d63cbbe8d6c63a7900c1d57d76
abdelrahman <a.m.hamza156@gmail.com>
2022-05-10 01:45:00 +0200

    process file

--- process.c
+++ process.c
@@ -3,18 +3,35 @@
 /* Modify this file as needed*/
 int remainingtime;
 
-int main(int agrc, char * argv[])
-{
+int main(int agrc, char *argv[]) {
     initClk();
-    
-    //TODO it needs to get the remaining time from somewhere
-    //remainingtime = ??;
-    while (remainingtime > 0)
-    {
-        // remainingtime = ??;
+    int previous_time, current_time;
+
+    // DONE: it needs to get the remaining time from somewhere
+    remainingtime = atoi(argv[1]);
+
+    // DONE: Set the start time
+    previous_time = getClk();
+
+    while (remainingtime > 0) {
+
+        // DONE: if the start time is less than the current time decrement the remaining time by one
+        current_time = getClk();
+
+        if (previous_time < current_time) {
+            remainingtime--;
+            previous_time = current_time;
+            // TODO: TESTING (Comment)
+            printf("Remaining time is %d\nPrevious time is %d \ncurrent time is %d\n", remainingtime, previous_time,
+                   current_time);
+        }
+
     }
-    
+
+    // DONE: When the remaining time is zero send a signal to the scheduler
+     kill(getppid(), SIGUSR2);
+
     destroyClk(false);
-    
+
     return 0;
 }
